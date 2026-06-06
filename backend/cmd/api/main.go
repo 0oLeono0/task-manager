277aeccf3eb06424ff29e7fc658fee2e5dcd0515
unknown <lyonya_ivanovskij@mail.ru>
2026-06-06 17:31:18 +0300

    feat: connect backend to postgres

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct {
@@ -14,17 +17,37 @@ type application struct {
 
 type config struct {
 	serverAddr string
+	dsn        string
 }
 
 func main() {
 	var cfg config
-	flag.StringVar(&cfg.serverAddr, "addr", ":8080", "HTTP server address")
+	logger := log.Default()
+	flag.StringVar(&cfg.serverAddr, "addr", ":8081", "HTTP server address")
+	flag.StringVar(&cfg.dsn, "db-dsn", "", "DSN")
 
 	flag.Parse()
+	if cfg.dsn == "" {
+		logger.Fatal("dsn is empty")
+	}
+
+	db, err := sql.Open("pgx", cfg.dsn)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Print("success connect to db")
 
 	tasks := sampleTasks()
 	app := &application{
-		logger: log.Default(),
+		logger: logger,
 		cfg:    cfg,
 		store: &taskStore{
 			tasks:  tasks,
